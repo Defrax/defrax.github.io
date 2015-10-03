81f5fabf8d8fe981c2347d5e29454fa7563b48a5
Defrax <d3f@gmail.com>
2015-10-03 23:27:38 +0300

    commit_for_host
    just creat_files

---
+++ script.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function(){
+    $(".portfolio-item img").mouseenter(function(){
+        $(".p-item-footer").hide();
+    });
+});
+
+$(document).ready(function(){
+    $(".portfolio-item img").mouseleave(function(){
+        $(".p-item-footer").show();
+    });
+});*/
